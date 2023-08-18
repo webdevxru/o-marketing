eb03b7a60536fa5ca186aef59347354b7bd9e6b7
webdevxru <webdevx@mail.ru>
2023-08-18 16:23:54 +0300

    Marckup without adaptive

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var mixer = mixitup('.portfolio__content', {
+
+    animation: {
+        "duration": 250,
+        "nudge": true,
+        "reverseOut": false,
+        "effects": "fade"
+    }
+});
+
+$('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"> <img src="/images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"> <img src="/images/arrow-right.svg" alt=""></button>',
+
+});
