d3335d8c974728b776a6316c2b4af01f84f04c31
webdevxru <webdevx@mail.ru>
2023-08-18 23:58:25 +0300

    Marckup + adaptive

--- js/main.js
+++ js/main.js
@@ -12,5 +12,16 @@ $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"> <img src="/images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"> <img src="/images/arrow-right.svg" alt=""></button>',
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false
+          }
+        },
 
-});
+]});
+
+$('.menu__btn').on('click', function(){
+    $('.header__top-inner').toggleClass('active');
+})
